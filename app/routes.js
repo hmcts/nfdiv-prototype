6f1a949765e888a2ea368071a6c52b3d0d065a41
Simon Meredith <simon@lerefue.co.uk>
2021-01-08 18:05:24 +0100

    Update journey, add in passing data and marriage date page

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 // Branching
-router.post('/postal_address', function (req, res) {
+router.post('/divorce/postal_address', function (req, res) {
   // Get the answer from session data
   // The name between the quotes is the same as the 'name' attribute on the input elements
   // However in JavaScript we can't use hyphens in variable names
@@ -10,13 +10,13 @@ router.post('/postal_address', function (req, res) {
   const over18 = req.session.data['broken-down']
 
   if (over18 === 'false') {
-    res.redirect('/cannot_divorce')
+    res.redirect('/divorce/cannot_divorce')
   } else {
-    res.redirect('/postal_address')
+    res.redirect('/divorce/postal_address')
   }
 })
 
-router.post('/email_address', function (req, res) {
+router.post('/divorce/marriage_date', function (req, res) {
   // Get the answer from session data
   // The name between the quotes is the same as the 'name' attribute on the input elements
   // However in JavaScript we can't use hyphens in variable names
@@ -24,9 +24,9 @@ router.post('/email_address', function (req, res) {
   const over18 = req.session.data['postal-address']
 
   if (over18 === 'false') {
-    res.redirect('/need_postal_address')
+    res.redirect('/divorce/need_postal_address')
   } else {
-    res.redirect('/email_address')
+    res.redirect('/divorce/marriage_date')
   }
 })
 
@@ -38,7 +38,7 @@ router.post('/sign_in', function (req, res) {
   const over18 = req.session.data['marriage-certificate']
 
   if (over18 === 'false') {
-    res.redirect('/no_marriage_cert')
+    res.redirect('/divorce/no_marriage_cert')
   } else {
     res.redirect('/sign_in')
   }
