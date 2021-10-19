c175aeabe152af537f0ea0350e8d752b68e14788
jamesros111 <81618904+jamesros111@users.noreply.github.com>
2021-10-19 10:34:43 +0100

    foupodates

--- app/routes.js
+++ app/routes.js
@@ -31,6 +31,22 @@ router.post('/divorce/marriage_date_2', function (req, res) {
 })
 
 
+router.post('/divorce/hub_7', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  const over18 = req.session.data['final']
+
+  if (over18 === 'Dispute') {
+    res.redirect('/divorce/final_order_not_apply')
+  } else {
+    res.redirect('/divorce/hub_7')
+  }
+})
+
+
+
 
 router.post('/divorce/review_application', function (req, res) {
   // Get the answer from session data
